d9376f45317db6460225336638d145f847ca5387
paulinhapaixao <159535749+luaninhadev@users.noreply.github.com>
2024-03-15 11:55:29 -0300

    maisumupdate
    gitando e mto feliz hahahahahaaaaaa

--- index.js
+++ index.js
@@ -12,10 +12,50 @@ console.log("o nome do aluno de melhor desempenho é: ", nomeDoAluno2); // ${} e
 console.log(typeof aluno3); // tipo da variável com "typeof"
 
 if (media > 7) {
-    console.log('A nota foi de ${media}. Parabéns')
+    console.log('A nota foi de ${media}. Parabéns');
 } else {
-    console.log('A média é menor que 5. Revise os conteúdos e prepare-se para a rec.')
+    console.log('A média é menor que 5. Revise os conteúdos e prepare-se para a rec.');
 }
+
+
+
+const idade = 17;
+
+if (idade >= 18) {
+    console.log("Você pode entrar.");
+} else {
+    console.log("Sua idade é menor que 18, retorne quando completar a maioridade.");
+}
+
+const pontos = 101;
+
+if (pontos > 100) {
+    console.log("Você receberá a bonificação de R$1000,00.");
+} else {
+    console.log("Conquiste 100 pontos para receber a bonificação.");
+}
+
+const entrada = 18;
+
+if (entrada == 18 || entrada > 18) {
+     console.log("Você pode entrar.");
+} 
+else {
+    console.log("Sua idade é menor que 18, retorne quando completar a maioridade.");
+}
+
+const carla = {
+    endereco: "Rua das árvores, 313 - Bairro do Limoeiro",
+    notaFinal: 10,
+    sala: 39,
+    turno: "Manhã"
+}
+console.log (carla.sala)
+
+// as chaves iniciam e terminam um objeto, já as vírgulas, separarão propriedades e os pontos acessarão as propriedades.
+
+const escola = [{},{},{},{}]
+
 
 
 
